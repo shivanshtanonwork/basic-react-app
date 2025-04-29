537062b4945f3d4fe6c4d6fdbe8b8628756f80a6
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-04-29 19:56:33 +0530

    Our 1st Component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Title from "./Title.jsx";
+
+function Description() {
+  return <h3>I am the Description!</h3>
+}
+function App() {
+  return (
+    <div>
+      <Title />
+      <Description />
+      <Title />
+      <Description />
+    </div>
+  );
+}
+
+export default App
