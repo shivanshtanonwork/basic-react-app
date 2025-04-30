6010b2e3e5821ee444e4258b9c1d21b77dfe71e1
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-04-30 23:06:17 +0530

    Event object

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Title from "./Title.jsx";
 import MsgBox from "./MsgBox.jsx";
 import ProductTab from "./ProductTab.jsx"
 import Button from "./Button.jsx"
+import Form from "./Form.jsx"
 
 function App() {
   return (
     <>
       {/* <MsgBox userName="Shivansh" txtColor="yellow" />
       <ProductTab /> */}
-      <Button />
+      {/* <Button /> */}
+      <Form />
     </>
   );
 }
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,6 @@
-function handleClick() {
+function handleClick(event) {
     console.log("Hello!");
+    console.log(event)
 
 }
 
